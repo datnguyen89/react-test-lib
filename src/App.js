b5d6c2031a78dffaee722a17dda2ad01b5f250a5
datnt <ntdat89.hn@gmail.com>
2024-01-14 21:10:58 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { ExampleComponent } from 'my-react-library-ntdat89hn'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ExampleComponent text="Create React Library Example 😄" />
+        </div>
+    );
 }
 
 export default App;
